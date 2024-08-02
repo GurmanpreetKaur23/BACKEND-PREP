426e6b0d5f82cf068c74f032bdd16f50305bbf10
GurmanpreetKaur23 <119606083+GurmanpreetKaur23@users.noreply.github.com>
2024-08-02 23:17:18 +0530

    Create script.js

---
+++ JAVASCRIPT/LEC-4/script.js
@@ -0,0 +1,35 @@
+// var x = 7 ;
+
+// function getName() {
+//     console.log("Namaste Javascript") ;
+// }
+
+// getName() ;
+// console.log(x) ;
+
+// Namaste Javascript
+// 7
+
+
+// ------------------------------
+
+// getName() ;
+// console.log(x) ; 
+// var x= 7 ;
+
+// function getName() {
+//     console.log("Namaste Javascript")
+// }
+
+// Namaste Javascript
+// undefined
+
+// ------------------------------
+
+// console.log(getName) ; 
+
+// function getName() {
+//     console.log("Namaste Javascript")
+// }
+
+// [Function: getName]
